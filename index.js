c1218eff428a6286b7115c00a875f57764b70f1c
Antoine Moevus <Antoine.Moevus@collegeahuntsic.qc.ca>
2023-06-29 11:12:39 -0400

    :apple: Add POST and GT routes

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const path = require('path')
 require('dotenv').config(); // Afin de charger le ficheir .env
 
-const Fruit = require("./models/Fruits.js")
 const routerV1 = require("./routers/apiV1Router.js")
 
 const app = express();
@@ -26,26 +25,6 @@ app.get('/', (req, res) => {
 });
 
 app.use("/api/v1", routerV1)
-
-// app.post("/api/v1/fruit", (req, res) => {
-
-//   //console.log("DB : ", req.body)
-//   // let data
-//   // if (Object.keys(req.body).length > 0) { // Tester si la requete a un body
-//   //    data = new Fruit(req.body)
-//   // } else {
-//   //  data = new Fruit(
-//   //   {
-//   //     nom: "lichi",
-//   //     poids: 100,
-//   //     couleur: "#a3a3a3",
-//   //     saveur: "sucrée"
-//   // })
-//   // }
-//   const data = new Fruit(req.body)
-//   data.save()
-//   res.send("success")
-// })
 
 const port = process.env.SERVER_PORT || 4200
 app.listen(port, () => {
--- routers/fruitRouter.js
+++ routers/fruitRouter.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
+const Fruit = require("../models/Fruits.js")
+
+
 // Définition des routes
-router.post('/', function(req, res) {
-  res.send('Vous avez fait une requête POST sur /chemin');
+router.post('/', function (req, res) {
+    if (Object.keys(req.body).length === 0) { // Tester si la requete a un body
+        res.end()
+    }
+    const data = new Fruit(req.body)
+    data.save()
+    res.send(JSON.stringify(data))
 });
 
-router.get('/:id', function(req, res) {
-  res.send('Vous avez fait une requête Get sur ' + req.params.id);
+
+router.get('/', (req, res) => {
+    Fruit.find().then(data => {
+    console.log("DB 77: ", data)
+    res.send(JSON.stringify(data))
+})
+});
+
+// router.get('/:key/:value', async (req, res) => {
+//     const key = String(req.params.key)
+//     console.log(key, req.params.value)
+//     const data = await Fruit.findOne({key : "pomme"}) // pourquoi ça marche pas??
+//     res.send(data)
+// });
+
+router.get('/poids/:value', async (req, res) => {
+    const data = await Fruit.findOne({poids : Number(req.params.value)})
+    res.send(data)
 });
 
 // Exportation du routeur
