1951a27ecc07854c8bbb9c8b685ff0355c80f5c9
Antoine Moevus <Antoine.Moevus@collegeahuntsic.qc.ca>
2023-06-29 10:45:33 -0400

    Add Fruit schema

---
+++ models/Fruits.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+
+const FruitSchema = mongoose.Schema(
+    {
+        nom: { type: String, required: true },
+        poids: { type: Number, required: true },
+        couleur: String,
+        saveur: String
+    }
+)
+
+
+module.exports = mongoose.model("fruits", FruitSchema)
