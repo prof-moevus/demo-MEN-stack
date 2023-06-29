3706da7785403b43dd8702c7f04af21ef70fa6a5
Antoine Moevus <Antoine.Moevus@collegeahuntsic.qc.ca>
2023-06-29 09:44:17 -0400

    :tortoise: Add boilerplate code for Express server

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path')
+require('dotenv').config(); // Afin de charger le ficheir .env
+
+const app = express();
+
+app.use(cors());
+
+// // Connection à MongoDB
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log("Connexion à MongoDB réussie !");
+//   })
+//   .catch((error) => {
+//     console.log("Erreur de connexion à MongoDB : " + error);
+//   });
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Bonjour monde !');
+});
+
+const port = process.env.SERVER_PORT || 4200
+app.listen(port, () => {
+  console.log(`Serveur démarré sur le port ${port}`);
+});
