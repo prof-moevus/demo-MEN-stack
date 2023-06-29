ad8a5b8532008f6d39a9ad93be30a00da9b7f09b
Antoine Moevus <Antoine.Moevus@collegeahuntsic.qc.ca>
2023-06-29 10:46:33 -0400

    Update Index for Fruit and use routers

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const cors = require('cors');
 const path = require('path')
 require('dotenv').config(); // Afin de charger le ficheir .env
 
-const app = express();
+const Fruit = require("./models/Fruits.js")
+const routerV1 = require("./routers/apiV1Router.js")
 
+const app = express();
 app.use(cors());
+app.use(express.json());
 
 // Connection à MongoDB
 mongoose.connect(process.env.ATLASDB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -17,11 +20,32 @@ mongoose.connect(process.env.ATLASDB_URI, { useNewUrlParser: true, useUnifiedTop
     console.log("Erreur de connexion à MongoDB : " + error);
   });
 
-app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Bonjour monde !');
 });
+
+app.use("/api/v1", routerV1)
+
+// app.post("/api/v1/fruit", (req, res) => {
+
+//   //console.log("DB : ", req.body)
+//   // let data
+//   // if (Object.keys(req.body).length > 0) { // Tester si la requete a un body
+//   //    data = new Fruit(req.body)
+//   // } else {
+//   //  data = new Fruit(
+//   //   {
+//   //     nom: "lichi",
+//   //     poids: 100,
+//   //     couleur: "#a3a3a3",
+//   //     saveur: "sucrée"
+//   // })
+//   // }
+//   const data = new Fruit(req.body)
+//   data.save()
+//   res.send("success")
+// })
 
 const port = process.env.SERVER_PORT || 4200
 app.listen(port, () => {
