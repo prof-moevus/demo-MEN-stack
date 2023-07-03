24b27d10b19deabb308033f181d93f70e4c40297
Antoine Moevus <Antoine.Moevus@collegeahuntsic.qc.ca>
2023-07-03 10:08:58 -0400

    📐 Add User model to webapp

--- routers/apiV1Router.js
+++ routers/apiV1Router.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const routerFruit = require("./fruitRouter")
+const routerUser = require("./userRouter")
+
 
 const router = express.Router();
 // Définition des routes
 router.use('/fruit', routerFruit);
+
+router.use('/auth', routerUser);
+
 
 
 // Exportation du routeur
